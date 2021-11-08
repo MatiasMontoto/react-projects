84a368d40e6369f6238da84325ba34daf37db122
MatiasMontoto <montoto.matias@yahoo.com.ar>
2021-11-08 18:03:14 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const Folder = ({ name, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const direction = isOpen ? "down" : "right";
+
+  const handleClick = () => setIsOpen(!isOpen);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span onClick={handleClick}>
+        <i className="green folder icon"></i>
+        <i className={`chevron ${direction} icon`}></i>
+      </span>
+      {name}
+      <div style={{ marginLeft: "16px" }}>{isOpen ? children : null}</div>
     </div>
   );
-}
+};
+
+const File = ({ name }) => {
+  const fileExtension = name.split(".")[1];
+  const fileIcons = {
+    mp4: "film",
+    mp3: "music",
+    jpeg: "file image",
+    png: "file image outline",
+  };
+  return (
+    <div>
+      <i className={`${fileIcons[fileExtension]} icon`}></i>
+      {name}
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <Folder name="Folder1">
+        <Folder name="SubFolder1">
+          <File name="audio.mp3" />
+          <File name="movie.mp4" />
+          <File name="image.jpeg" />
+        </Folder>
+        <File name="somefile.png" />
+      </Folder>
+      <Folder name="Folder2">
+        <File name="anotherfile" />
+      </Folder>
+    </div>
+  );
+};
 
 export default App;
